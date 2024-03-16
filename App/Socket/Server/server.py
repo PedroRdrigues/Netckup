1e79230b2b52b76caddad10e116bf824802da90c
ZorcKs <p.hrdrigues1@gmail.com>
2024-03-16 14:03:03 -0300

    Modificações no arquivo client.py.

--- App/Socket/Server/server.py
+++ App/Socket/Server/server.py
@@ -2,30 +2,40 @@ import os
 import socket
 
 
-HOST = "192.168.100.73"
-PORT = 5500
+# Realiza a inicialização do servidor e aguarda a conexão do cliente.
+def main():
+  HOST = "192.168.100.73"
+  PORT = 50501
 
-SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-SERVER.bind((HOST,PORT))
-SERVER.listen(1); print(">>> Esperando conexão...")
+  SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  SERVER.bind((HOST,PORT))
+  SERVER.listen(); print(">>> Esperando conexão...\n")
 
-conn , addrs = SERVER.accept()
+  conn , addrs = SERVER.accept()
 
-res = conn.recv(1024).decode()
+"""
+Cria funções para receber a requisição e enviar uma resposta a partir dos códigos a baixo.
+"""
 
-if os.path.isfile(res):
-  
-  with open(res, 'rb') as file:
-    for data in file.readlines():
-      conn.send(data)
+
+
+inicio = conn.recv(1024).decode()
+if inicio == 'init':
+  res = 'archive.zip'
+
+  if os.path.isfile(res):
+    print(f">>> {res} requisitado...\n")
     
-    print(f"\n>>> {res} enviado...")
+    with open(res, 'rb') as file:
+      for data in file.readlines():
+        conn.send(data)
+      
+      print(f"\n>>> {res} enviado...\n")
 
-elif os.path.isdir(res):
-    print("\n>>> isso é um diretório!")
+  elif os.path.isdir(res):
+      print("\n>>> isso é um diretório!\n")
 
-else:
-  print("\n>>> Dados não identificado!")
+  else:
+    print("\n>>> Dados não identificado!\n")
 
-print("\n>>> Fim...")
-# os.system('pause')
+  print("\n>>> Fim...\n")
